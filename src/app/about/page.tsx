10cc39effff72e328d1fc981da5b61b27d79230c
Maciej Kaczanowski <maciej.emil.kaczanowski@gmail.com>
2023-09-15 14:00:44 +0200

    feat(about page): init
    add dummy about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>About page</h1>
+      <div>Details about me</div>
+        <div/>
+    </main>
+  )
+}
